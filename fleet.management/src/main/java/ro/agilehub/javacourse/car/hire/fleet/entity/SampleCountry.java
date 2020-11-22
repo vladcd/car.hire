105d26dd9e3bc0e9f115fe8e58f2201984c6f506
vladcd <vladcarcu@gmail.com>
2020-11-22 16:46:43 +0200

    Lesson 5 - setup lombok

--- fleet.management/src/main/java/ro/agilehub/javacourse/car/hire/fleet/entity/SampleCountry.java
+++ fleet.management/src/main/java/ro/agilehub/javacourse/car/hire/fleet/entity/SampleCountry.java
@@ -1,10 +1,12 @@
 package ro.agilehub.javacourse.car.hire.fleet.entity;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
+@Data
+@EqualsAndHashCode(of = "id")
 @Entity
 @Table(name = "country")
 public class SampleCountry {
@@ -17,47 +19,4 @@ public class SampleCountry {
 
     private String isoCode;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getIsoCode() {
-        return isoCode;
-    }
-
-    public void setIsoCode(String isoCode) {
-        this.isoCode = isoCode;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-
-        if (o == null || getClass() != o.getClass()) return false;
-
-        SampleCountry that = (SampleCountry) o;
-
-        return new EqualsBuilder()
-                .append(id, that.id)
-                .isEquals();
-    }
-
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder(17, 37)
-                .append(id)
-                .toHashCode();
-    }
 }
--- fleet.management/src/main/java/ro/agilehub/javacourse/car/hire/fleet/entity/SampleEntity.java
+++ fleet.management/src/main/java/ro/agilehub/javacourse/car/hire/fleet/entity/SampleEntity.java
@@ -1,12 +1,14 @@
 package ro.agilehub.javacourse.car.hire.fleet.entity;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
+@Data
+@EqualsAndHashCode(of = "id")
 @Entity
 @Table(name = "sample_entity")
 public class SampleEntity {
@@ -24,47 +26,4 @@ public class SampleEntity {
     @LastModifiedDate
     private LocalDate joinDate;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public SampleCountry getCountry() {
-        return country;
-    }
-
-    public void setCountry(SampleCountry country) {
-        this.country = country;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-
-        if (o == null || getClass() != o.getClass()) return false;
-
-        SampleEntity that = (SampleEntity) o;
-
-        return new EqualsBuilder()
-                .append(id, that.id)
-                .isEquals();
-    }
-
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder(17, 37)
-                .append(id)
-                .toHashCode();
-    }
 }
